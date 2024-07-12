792e5e4740c14ec474aba147a5afdea5df89c785
alenjb <alenjb@naver.com>
2024-07-13 02:24:26 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/cobin/lottopilot/LottopilotApplication.java
@@ -0,0 +1,13 @@
+package com.cobin.lottopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottopilotApplication.class, args);
+	}
+
+}
